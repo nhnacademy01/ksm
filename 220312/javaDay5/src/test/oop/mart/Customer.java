6968b3b0226d721727b04ab1efec33394458c1d9
SemiKimm <semikimm.m@gmail.com>
2022-03-13 19:59:36 +0900

    Customer.addToBasket() : FoodStand.getFoodToBasket() 으로 메서드 분리

--- 220312/javaDay5/src/test/oop/mart/Customer.java
+++ 220312/javaDay5/src/test/oop/mart/Customer.java
@@ -1,7 +1,5 @@
 package test.oop.mart;
 
-import java.util.Scanner;
-
 public class Customer {
     private final Wallet wallet;
     private final BuyList buyList;
@@ -21,23 +19,7 @@ public class Customer {
     }
 
     public void addToBasket(FoodStand foodStand) {
-        //일단 동작 제대로 되는듯 그런데 입력한 수량이 매대의 수량보다 많을 경우예외 처리
-        // 객체지향적으로 코드 정리하기(메서드 분리하기)
-        for (BuyList.Item item : buyList.items) {
-            int addFoodCounts = 0;
-            for (Food food : foodStand.foods) {
-                if (food.isSame(item)) {
-                    if (addFoodCounts >= item.getAmount()) {
-                        break;
-                    }
-                    basket.add(food);
-                    addFoodCounts++;
-                }
-            }
-            if(addFoodCounts<item.getAmount()){
-                System.out.printf("매장에 있는 %s 가 희망하는 구매 수량보다 적습니다. %d 개만 장바구니에 담습니다.%n",item.getName(),addFoodCounts);
-            }
-        }
+        foodStand.getFoodToBasket(this.buyList,this.basket);
         basket.print();
     }
 
